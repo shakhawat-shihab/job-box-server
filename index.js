bb1b3e7e6453234be4074014a6148751212a9292
SHAKHAWAT <shihabchtg@gmail.com>
2023-01-13 21:55:07 +0600

    bug fix

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zwiso.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const uri = `mongodb://localhost:27017`
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zwiso.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+// const uri = `mongodb://localhost:27017`
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
